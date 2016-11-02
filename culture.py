70d4d1a1da6c4bbdd38923cb9e31e2d3f52b47ad
mehmettozen <mehmettozen@gmail.com>
2016-11-02 16:35:48 +0200

    insert operation is added with post method

--- culture.py
+++ culture.py
@@ -4,7 +4,6 @@ from settings import *
 def culture_page():
     with dbapi2.connect(app.config['dsn']) as connection:
         with connection.cursor() as cursor:
-        #cursor = connection.cursor()
             query = """SELECT * FROM Culture"""
             cursor.execute(query)
             culture_data = json.dumps(cursor.fetchall())
@@ -17,8 +16,16 @@ def culture_page():
 def culture_details(name):
     return render_template('culture_details.html', name=name)
 
-@app.route('/culture/insert')
+@app.route('/culture/insert', methods=["POST"])
 def culture_insert():
+    name = request.form['cultural_place_name']
+    score = request.form['cultural_place_score']
+    with dbapi2.connect(app.config['dsn']) as connection:
+        with connection.cursor() as cursor:
+            statement = """INSERT INTO Culture (NAME, SCORE)
+                        VALUES (%s, %s)"""
+            cursor.execute(statement, (name,score))
+
     return redirect(url_for('culture_page'))
 
 @app.route('/culture/delete')
@@ -27,4 +34,13 @@ def culture_delete():
 
 @app.route('/culture/update')
 def culture_update():
+    return redirect(url_for('culture_page'))
+
+@app.route('/culture/delete_all')
+def culture_delete_all():
+    with dbapi2.connect(app.config['dsn']) as connection:
+        with connection.cursor() as cursor:
+            query = """DELETE FROM Culture"""
+            cursor.execute(query)
+
     return redirect(url_for('culture_page'))
