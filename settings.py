061688b1a02ccd27d7fe1de31f7cab96bf300683
mehmettozen <mehmettozen@gmail.com>
2016-11-02 16:34:19 +0200

    request is imported from flask

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ import re
 from flask import Flask
 from flask import redirect
 from flask import render_template
+from flask import request
 from flask.helpers import url_for
 
 app = Flask(__name__)
