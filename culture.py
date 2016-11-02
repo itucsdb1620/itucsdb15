2fec88f5f4106626b45e220e7ac0c9357a978458
mehmettozen <mehmettozen@gmail.com>
2016-11-02 18:40:53 +0200

    delete and update functionality added, "culture_details" function now uses a query result

--- culture.py
+++ culture.py
@@ -12,9 +12,15 @@ def culture_page():
     now = datetime.datetime.now()
     return render_template('culture.html', current_time=now.ctime(), culture=culture)
 
-@app.route('/culture/<string:name>')
-def culture_details(name):
-    return render_template('culture_details.html', name=name)
+@app.route('/culture/<int:id>')
+def culture_details(id):
+    with dbapi2.connect(app.config['dsn']) as connection:
+        with connection.cursor() as cursor:
+            statement = """SELECT * FROM Culture WHERE (ID = %s)"""
+            cursor.execute(statement, (id,))
+            culture_data = json.dumps(cursor.fetchall())
+            culture = json.loads(culture_data)
+    return render_template('culture_details.html', culture=culture)
 
 @app.route('/culture/insert', methods=["POST"])
 def culture_insert():
@@ -28,12 +34,29 @@ def culture_insert():
 
     return redirect(url_for('culture_page'))
 
-@app.route('/culture/delete')
+@app.route('/culture/delete', methods=["POST"])
 def culture_delete():
+    id = request.form["select"]
+    with dbapi2.connect(app.config['dsn']) as connection:
+        with connection.cursor() as cursor:
+            statement = """DELETE FROM Culture WHERE ID = (%s)"""
+            cursor.execute(statement, (id))
+
     return redirect(url_for('culture_page'))
 
-@app.route('/culture/update')
+@app.route('/culture/update', methods=["POST"])
 def culture_update():
+    name = request.form["cultural_name_update"]
+    score = request.form["cultural_score_update"]
+    id = request.form["cultural_index"]
+    with dbapi2.connect(app.config['dsn']) as connection:
+        with connection.cursor() as cursor:
+            if name:
+                statement = """UPDATE Culture SET (NAME) = (%s) WHERE (ID = %s)"""
+                cursor.execute(statement, (name,id))
+            if score:
+                statement = """UPDATE Culture SET (SCORE) = (%s) WHERE (ID = %s)"""
+                cursor.execute(statement, (score,id))
     return redirect(url_for('culture_page'))
 
 @app.route('/culture/delete_all')
