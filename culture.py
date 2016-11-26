67be192e76193bf63b7575f2e09aacf4890111c0
mehmettozen <mehmettozen@gmail.com>
2016-11-26 21:40:50 +0200

    new attributes added to culture table. new data inserted into the table according to new attributes

--- culture.py
+++ culture.py
@@ -26,11 +26,12 @@ def culture_details(id):
 def culture_insert():
     name = request.form['cultural_place_name']
     score = request.form['cultural_place_score']
+    info = request.form['cultural_place_info']
     with dbapi2.connect(app.config['dsn']) as connection:
         with connection.cursor() as cursor:
-            statement = """INSERT INTO Culture (NAME, SCORE)
-                        VALUES (%s, %s)"""
-            cursor.execute(statement, (name,score))
+            statement = """INSERT INTO Culture (NAME, SCORE, INFO)
+                        VALUES (%s, %s, %s)"""
+            cursor.execute(statement, (name,score,info))
 
     return redirect(url_for('culture_page'))
 
@@ -47,17 +48,17 @@ def culture_delete():
 @app.route('/culture/update', methods=["POST"])
 def culture_update():
     name = request.form["cultural_name_update"]
-    score = request.form["cultural_score_update"]
+    photo = request.form["cultural_photo_update"]
     id = request.form["cultural_index"]
     with dbapi2.connect(app.config['dsn']) as connection:
         with connection.cursor() as cursor:
             if name:
                 statement = """UPDATE Culture SET (NAME) = (%s) WHERE (ID = %s)"""
                 cursor.execute(statement, (name,id))
-            if score:
-                statement = """UPDATE Culture SET (SCORE) = (%s) WHERE (ID = %s)"""
-                cursor.execute(statement, (score,id))
-    return redirect(url_for('culture_page'))
+            if photo:
+                 statement = """UPDATE Culture SET PHOTO = (%s) WHERE (ID = %s)"""
+                 cursor.execute(statement, (photo,id))
+    return redirect(url_for('culture_details',id=id))
 
 @app.route('/culture/delete_all')
 def culture_delete_all():
@@ -67,3 +68,16 @@ def culture_delete_all():
             cursor.execute(query)
 
     return redirect(url_for('culture_page'))
+
+@app.route('/culture/vote', methods=['POST'])
+def culture_voting():
+    vote = request.form["vote"]
+    id = request.form["cultural_index2"]
+    with dbapi2.connect(app.config['dsn']) as connection:
+        with connection.cursor() as cursor:
+            if vote:
+                statement = """UPDATE Culture SET SCORE = (SCORE * VOTES + %s) / (VOTES+1),
+                            VOTES = VOTES + 1 WHERE (ID = %s)"""
+                cursor.execute(statement, (vote,id))
+
+    return redirect(url_for('culture_details', id=id))
