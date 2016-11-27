8ffd47cb00d85aaef0912fcd7ea2245cb3e9dd77
mehmettozen <mehmettozen@gmail.com>
2016-11-27 14:50:03 +0200

    now a left join is executed for select operation on foreign key relation. activity_id attribute can be inserted and updated for culture table

--- culture.py
+++ culture.py
@@ -4,34 +4,62 @@ from settings import *
 def culture_page():
     with dbapi2.connect(app.config['dsn']) as connection:
         with connection.cursor() as cursor:
-            query = """SELECT * FROM Culture"""
+            query = """SELECT Culture.ID, Culture.NAME, Culture.SCORE,
+                        Culture.VOTES, Culture.INFO, Culture.PHOTO,
+                        Culture.ACTIVITY_ID, Activities.NAME FROM Culture
+                        LEFT OUTER JOIN Activities
+                        ON Culture.ACTIVITY_ID=Activities.ID"""
             cursor.execute(query)
             culture_data = json.dumps(cursor.fetchall())
             culture = json.loads(culture_data)
 
+            query = """SELECT ID,NAME FROM Activities"""
+            cursor.execute(query)
+            activity_data = json.dumps(cursor.fetchall())
+            activities = json.loads(activity_data)
+
     now = datetime.datetime.now()
-    return render_template('culture.html', current_time=now.ctime(), culture=culture)
+    return render_template('culture.html', current_time=now.ctime(), culture=culture, activities=activities)
 
 @app.route('/culture/<int:id>')
 def culture_details(id):
     with dbapi2.connect(app.config['dsn']) as connection:
         with connection.cursor() as cursor:
-            statement = """SELECT * FROM Culture WHERE (ID = %s)"""
+            statement = """SELECT Culture.ID, Culture.NAME, Culture.SCORE,
+                        Culture.VOTES, Culture.INFO, Culture.PHOTO,
+                        Culture.ACTIVITY_ID, Activities.NAME FROM Culture
+                        LEFT OUTER JOIN Activities
+                        ON Culture.ACTIVITY_ID=Activities.ID WHERE (Culture.ID = %s)"""
             cursor.execute(statement, (id,))
             culture_data = json.dumps(cursor.fetchall())
             culture = json.loads(culture_data)
-    return render_template('culture_details.html', culture=culture)
+
+            query = """SELECT ID,NAME FROM Activities"""
+            cursor.execute(query)
+            activity_data = json.dumps(cursor.fetchall())
+            activities = json.loads(activity_data)
+
+    return render_template('culture_details.html', culture=culture, activities=activities)
 
 @app.route('/culture/insert', methods=["POST"])
 def culture_insert():
     name = request.form['cultural_place_name']
     score = request.form['cultural_place_score']
+    votes = request.form['cultural_place_votes']
     info = request.form['cultural_place_info']
+    photo = request.form['cultural_place_photo']
+    activity = request.form['cultural_activity_id']
     with dbapi2.connect(app.config['dsn']) as connection:
         with connection.cursor() as cursor:
-            statement = """INSERT INTO Culture (NAME, SCORE, INFO)
-                        VALUES (%s, %s, %s)"""
-            cursor.execute(statement, (name,score,info))
+            if name and score and votes and activity:
+                query = """SELECT * FROM Activities WHERE (ID = %s)"""
+                cursor.execute(query, (activity,))
+                exists_data = json.dumps(cursor.fetchall())
+                exists = json.loads(exists_data)
+                if(exists):
+                    statement = """INSERT INTO Culture (NAME, SCORE, VOTES, INFO, PHOTO, ACTIVITY_ID)
+                        VALUES (%s, %s, %s, %s, %s, %s)"""
+                    cursor.execute(statement, (name,score,votes,info,photo,activity))
 
     return redirect(url_for('culture_page'))
 
@@ -49,6 +77,8 @@ def culture_delete():
 def culture_update():
     name = request.form["cultural_name_update"]
     photo = request.form["cultural_photo_update"]
+    info = request.form["cultural_info_update"]
+    activity = request.form["culture_activity_update"]
     id = request.form["cultural_index"]
     with dbapi2.connect(app.config['dsn']) as connection:
         with connection.cursor() as cursor:
@@ -56,8 +86,14 @@ def culture_update():
                 statement = """UPDATE Culture SET (NAME) = (%s) WHERE (ID = %s)"""
                 cursor.execute(statement, (name,id))
             if photo:
-                 statement = """UPDATE Culture SET PHOTO = (%s) WHERE (ID = %s)"""
-                 cursor.execute(statement, (photo,id))
+                statement = """UPDATE Culture SET PHOTO = (%s) WHERE (ID = %s)"""
+                cursor.execute(statement, (photo,id))
+            if info:
+                statement = """UPDATE Culture SET INFO = (%s) WHERE (ID = %s)"""
+                cursor.execute(statement, (info,id))
+            if activity:
+                statement = """UPDATE Culture SET ACTIVITY_ID = (%s) WHERE (ID = %s)"""
+                cursor.execute(statement, (activity,id))
     return redirect(url_for('culture_details',id=id))
 
 @app.route('/culture/delete_all')
